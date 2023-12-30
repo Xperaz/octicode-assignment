45e9d3bd8883a1b9e5e64b3169b194cc358536b7
xperaz <aouhadou@student.1337.ma>
2023-12-30 16:22:34 +0100

    Dockerize Application

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const usersController = require('./controllers/getUsersController');
 const findUserController = require('./controllers/findUserController');
 
 const server = http.createServer(async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
     if (req.url === '/api/users' && req.method === 'GET'){
         usersController.getUsers(req, res);
     }
